6a11b1a451982658a2739b2a387771878be4ec86
GroverZhu <groverzhu@gmail.com>
2018-10-21 20:10:34 +0800

    修复请求过滤器中的将图片过滤的错误

--- Ex5/src/ex5/RequestFilter.java
+++ Ex5/src/ex5/RequestFilter.java
@@ -32,25 +32,46 @@ public class RequestFilter implements Filter {
 	public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain)
 			throws IOException, ServletException {
 		// 用于判断是否满足SelectBeer.jsp发送的Get请求，
-		boolean flag = true;
+		boolean flag = false;
+		// 用于判断是否符合资源路径，满足指定的jsp, .png文件
+		boolean resFlag = false;
+
 		HttpServletRequest httpRequest = (HttpServletRequest) request;
 		HttpServletResponse httpResponse = (HttpServletResponse) response;
+
 		// 获取重定向路径(当前application的路径+init-param中的初始化的值)
 		String redirectPath = httpRequest.getContextPath() + config.getInitParameter("redirectPath");
-		System.out.println("-*-" + redirectPath + "-*-");
-		// 判断request是否是get方法，且不是由SelectBeer.jsp发送的
-		// 如果满足上述条件,则需要跳转
-		if (httpRequest.getMethod().toUpperCase().equals("GET") && 
-				httpRequest.getRequestURI().indexOf(redirectPath) == -1) {
-				flag = false;
-		}
-		System.out.println("-*-" + flag + "-*-");
-		// 根据flag的值是否满足跳转条件
-		if (flag) {
+		String includeStrings = config.getInitParameter("includeStrings");
+		String disableFilter = config.getInitParameter("disableFilter");
+
+		String[] includeList = includeStrings.split(";");
+
+		System.out.println("-*-\n" + redirectPath + "\n" + includeStrings + "\n" + disableFilter + "\n-*-");
+		// 判断初始化参数是否让该Filter生效
+		if (disableFilter.toUpperCase().equals("Y")) {
 			chain.doFilter(request, response);
-			return;
-		} else {
-			httpResponse.sendRedirect(redirectPath);
+		}
+		if (disableFilter.toUpperCase().equals("N")) {
+			// 判断是否符合资源要求
+			for (int i = 0; i < includeList.length; i++) {
+				if (httpRequest.getRequestURI().indexOf(includeList[i]) != -1) {
+					resFlag = true;
+					break;
+				}
+			}
+			// 判断request是否是get方法
+			if (!httpRequest.getMethod().toUpperCase().equals("GET")) {
+				flag = true;
+			}
+			System.out.println("-*+\n" + resFlag + "\n" + flag + "+*-");
+			// 根据flag 与 resFlag 的值是否满足跳转条件
+			// 有条件可知SelectBeer.jsp中flag=false， resFlag=true
+			if (flag || resFlag) {
+				chain.doFilter(request, response);
+				return;
+			} else {
+				httpResponse.sendRedirect(redirectPath);
+			}
 		}
 
 	}
